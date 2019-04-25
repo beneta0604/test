a4146f93f2cd8de908d5f87ed6e0fb204b3f1bbb
beneta0604 <43533829+beneta0604@users.noreply.github.com>
2019-04-25 14:18:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,44 +1,36 @@
-def count(mustbeSafedSymbols,symbols):
-    #подсчитывает символы
-    amount = []
-    for index in mustbeSafedSymbols:
-        amount.append(symbols.count(index))
-    amount[2]=amount[2]+amount[3]
-    amount.pop(3)
-    return(amount)
 
-def collectSymbols(mustbeSafedSymbols):
-    # собирает все нужные введённые символы.
-    symbols = []
+
+amount = []
+symbols = []
+mustbeSafedSymbols = ['3', '7', 'K', 'k']
+
+#сахраняет нужные введённые символы
+symbol = input("введите любой символ")
+while symbol != '0':
+    if symbol == '0':
+        break
+    elif symbol == mustbeSafedSymbols[0] or symbol == mustbeSafedSymbols[1] or symbol == mustbeSafedSymbols[2] or symbol == mustbeSafedSymbols[3]:
+        symbols.append(symbol)
     symbol = input("введите любой символ")
 
-    while symbol != '0':
-        if symbol == '0':
-            break
-        elif symbol == mustbeSafedSymbols[0] or symbol == mustbeSafedSymbols[1] or symbol == mustbeSafedSymbols[2] or symbol == mustbeSafedSymbols[3]:
-            symbols.append(symbol)
-        symbol = input("введите любой символ")
-    return(symbols)
+#подсчитывает символы
+for index in mustbeSafedSymbols:
+    amount.append(symbols.count(index))
+amount[2]=amount[2]+amount[3]
+amount.pop(3)
 
-def hystogram():
-    #ресует гистограмму
-    mustbeSafedSymbols = ['3', '7', 'K', 'k']
-    symbols=collectSymbols(mustbeSafedSymbols)
-    amount=count(mustbeSafedSymbols,symbols)
-
-
-    for index in range(len(amount)):
-        counter=0
-        if mustbeSafedSymbols[index]=='K':
-            print("K,   k", end='')
-        else:
-            print(mustbeSafedSymbols[index],"  ",amount[index], end='')
-        if amount[index]>9:
-            print("   ",end='')
-        else:
-            print("    ",end='')
-        while counter!=amount[index]:
-            print('*', end='')
-            counter+=1
-        print('\n')
-hystogram()
+#ресует гистограмму
+for index in range(len(amount)):
+    counter=0
+    if mustbeSafedSymbols[index]=='K':
+        print("K,   k", end='')
+    else:
+        print(mustbeSafedSymbols[index],"  ",amount[index], end='')
+    if amount[index]>9:
+        print("   ",end='')
+    else:
+        print("    ",end='')
+    while counter!=amount[index]:
+        print('*', end='')
+        counter+=1
+    print('\n')
